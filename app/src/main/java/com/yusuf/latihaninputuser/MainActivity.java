ed0eb9e2643efffeb83b5dc1f0ed0399bed65053
yusufdy <yusufdy04@gmail.com>
2020-09-03 10:50:42 +0700

    latihan

---
+++ app/src/main/java/com/yusuf/latihaninputuser/MainActivity.java
@@ -0,0 +1,30 @@
+package com.yusuf.latihaninputuser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    ArrayList<String> list = new ArrayList<String>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView=(ListView)findViewById(R.id.ListView);
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(arrayAdapter);
+
+        list.add("RPL");
+        list.add("TKJ");
+        list.add("MM");
+        list.add("AVI");
+        list.add("TOI");
+    }
+}
